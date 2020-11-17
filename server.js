3580b3645f860cf20dc781836d0c88aeeb38c347
Glitch (wry-brook-provelone) <none>
2020-11-17 16:13:39 +0000

    🛣🐑 Checkpoint ./server.js:24662156/1710

--- server.js
+++ server.js
@@ -23,6 +23,26 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
@@ -39,10 +59,15 @@ const userSchema = mongoose.Schema({
 })
 const USER = mongoose.model("USER", userSchema);
 
-app.get("/api", (req, res) => {
-  res.json({data: "user submit"});
+// add user
+app.post("/api/exercise/new-user", (req, res) => {
+  var userName = req.body.username;
+  var user = USER.findOne({username: user})
+  console.log(user)
+  res.send('user')
 });
 
+// add exercise
 app.post("/api/exercise/add", (req, res) => {
   res.send("exercises");
 });
