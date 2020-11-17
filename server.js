c786bb2831c007fe44b3968bddb5fb3a55fa5861
Glitch (wry-brook-provelone) <none>
2020-11-17 16:00:52 +0000

    🤘🌼 Checkpoint ./server.js:24662156/1219

--- server.js
+++ server.js
@@ -23,31 +23,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
@@ -64,8 +39,8 @@ const userSchema = mongoose.Schema({
 })
 const USER = mongoose.model("USER", userSchema);
 
-app.post("/api/exercise/new-user", (req, res) => {
-  res.send("user submit");
+app.get("/api", (req, res) => {
+  res.json({data: "user submit"});
 });
 
 app.post("/api/exercise/add", (req, res) => {
